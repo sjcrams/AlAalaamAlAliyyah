68b6e2e8b54b2e236fb52b879a395582fd63a1c1
sjcrams <32602102+aznameusacom@users.noreply.github.com>
2017-10-16 16:34:29 +0300

    Initial commit

---
+++ mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"الأعلام العلية في مناقب شيخ الإسلام ابن تيمية",page:"1",url:"",level:"1",children:[{caption:"المقدمة",page:"5",url:"",level:"2",children:[]},{caption:"النص المحقق",page:"18",url:"",level:"2",children:[]},{caption:"الفهرس",page:"89",url:"",level:"2",children:[]}]}];
